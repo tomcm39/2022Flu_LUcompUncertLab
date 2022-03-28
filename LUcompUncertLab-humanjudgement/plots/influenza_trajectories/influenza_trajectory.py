70886a8c08e8b4841380606c229952bbefd87d19
tom mcandrew <mcandrew@lehigh.edu>
2022-03-28 08:07:19 -0400

    Forecasts generated for LUcompUncertLab team on 2022-03-28

---
+++ LUcompUncertLab-humanjudgement/plots/influenza_trajectories/influenza_trajectory.py
@@ -0,0 +1,16 @@
+#mcandrew
+
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+if __name__ == "__main__":
+
+    fludata = pd.read_csv("../../../../data-truth/truth-Incident Hospitalizations.csv")
+
+    
+
+    
+
